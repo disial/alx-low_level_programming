a696b113c3b3b9cd332e579a907ffbb16e24c3b8
disial <144545367+disial@users.noreply.github.com>
2023-10-07 16:39:25 +0000

    first c function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include
+/***
+* main - Return a string using puts() function
+* rreturn: returns null
+*/
+int main(void)
+{
+    puts("\"Peogramming is like building a multilingual puzzle")
+    return (0);
+}
