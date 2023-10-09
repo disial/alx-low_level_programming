21ff0287620df18fbb38e34c457089a40a7006c4
disial <144545367+disial@users.noreply.github.com>
2023-10-09 18:07:44 +0000

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * Return: 0
+ * n: int type
+ */
+int main(void)
+{
+	int n = 0;
+
+	for (; n <= 9; n++)
+		printf("%d", n);
+	printf("\n");
+	return (0);
+}
