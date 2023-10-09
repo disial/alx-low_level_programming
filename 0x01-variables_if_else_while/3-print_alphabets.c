a35f59ac524377d74a370ac3ecd37b8a5c8ab899
disial <144545367+disial@users.noreply.github.com>
2023-10-09 16:59:43 +0000

    alpha

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - function main
+ * Return: 0
+ * alpha_lower: type char
+ * alpha_upper: type char
+ */
+int main(void)
+{
+	char alpha_lower = 'a';
+	char alpha_upper = 'A';
+
+	while (alpha_lower <= 'z')
+	{
+		putchar(alpha_lower);
+		alpha_lower++;
+	}
+	while (alha_upper <= 'Z')
+	{
+		putchar(alpha_upper);
+		alpha_upper++;
+	}
+	putchar('\n');
+	return (0);
+}
