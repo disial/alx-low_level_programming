8ff8a2426940eaf1f1b3239052f5730cdbae61c5
disial <144545367+disial@users.noreply.github.com>
2023-10-09 17:57:32 +0000

    alphabet

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * Return: 0
+ * ch: type char
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if (ch != 'e' && ch != 'q')
+			putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
