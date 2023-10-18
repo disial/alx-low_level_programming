f0111e5f00bcb95934fb31f0deb2401ba1f5d4e1
disial <144545367+disial@users.noreply.github.com>
2023-10-18 19:35:06 +0000

    str conct

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strncat - concat str
+ * @dest: pointer str
+ * @src: str pointer
+ * @n: str
+ * Return: dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	int j = 0;
+
+	for (; dest[i] != '\0';)
+		i++;
+	for (; src[j] != '\0' && n > 0;)
+	{
+		j++, n--, i++;
+		dest[i] = src[j];
+	}
+	return (dest);
+}
