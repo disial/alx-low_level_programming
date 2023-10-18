b241f9e375ca0a4f922b867c67dda034386709d4
disial <144545367+disial@users.noreply.github.com>
2023-10-18 19:24:21 +0000

    str cut

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,7 +11,8 @@ char *_strcat(char *dest, char *src)
 	int i = 0;
 	int j = 0;
 
-	for (; dest[i] != '\0'; i++);
+	for (; dest[i] != '\0';)
+		i++;
 	for (; src[j] != '\0'; j++)
 	{
 		dest[i] = src[j];
