8162fee7404041f1802e2ab118db8a3d3255bee7
disial <144545367+disial@users.noreply.github.com>
2023-10-18 19:47:04 +0000

    copy str

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strncpy - function o copy str
+ * @dest: str pointer 
+ * @src: str pointer
+ * @n: str
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	for (; i < n && src[i] != '\0';)
+	{
+		i++;
+		dest[i] = src[i];
+	}
+	for (; n > i;)
+	{
+		i++;
+		dest[i] = '\0';
+	}
+	return (dest);
+}
