fbfa9575211bebc8598ebbea62c12d7af2290ee3
disial <144545367+disial@users.noreply.github.com>
2023-11-12 15:57:31 +0000

    argc argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - main func
+ * @argc:  var count
+ * @argv: arg vector
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	argc++;
+	printf("%s\n", argv[0]);
+	return (0);
+}
