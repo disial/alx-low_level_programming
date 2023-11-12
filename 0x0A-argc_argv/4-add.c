9f2285e7f1c4f82fe1cef0c89005eb42d0fdd458
disial <144545367+disial@users.noreply.github.com>
2023-11-12 18:31:59 +0000

    add

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main func
+ * @argc:  var count
+ * @argv: arg vector
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i = 0;
+	int sum;
+	int tot;
+
+	if (argc == 1)
+	{
+		printf("%d\n", argc - 1);
+		return (0);
+	}
+	else if (argc > 1)
+	{
+		sum = 0;
+		for (i = 1; i <= argc - 1; i++)
+		{
+			if (atoi(argv[i]) == 0 && argv[i][0] != '0')
+			{
+				printf("Error\n");
+				return (0);
+			}
+			else
+			{
+				sum += atoi(argv[i]);
+				tot = sum;
+			}
+		}
+		printf("%d\n", tot);
+		return (0);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+}
