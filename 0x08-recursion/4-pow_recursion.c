983fd8d7ff5d484a35fa4b4fbd4844d6a0cd6bfb
disial <144545367+disial@users.noreply.github.com>
2023-11-12 11:42:32 +0000

    power

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - power func
+ * @x: int num
+ * @y: int num
+ * Return: power or -1
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (0);
+	if (y == 1)
+		return (x);
+	else
+		return (x * _pow_recursion(x, y - 1));
+}
