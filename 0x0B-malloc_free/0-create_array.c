de0291728c26b19c6effb4b878643e707fcae43f
disial <144545367+disial@users.noreply.github.com>
2023-11-14 20:32:12 +0000

    str pointer

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * create_array - pointer func
+ * @c: pointer char
+ * @size: of array
+ * Return: NULL
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *str = malloc(sizeof(char) * size);
+	unsigned int i;
+
+	if (size == 0)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		str[i] = c;
+	return (str);
+}
