4e5c1afc4cf275f35e7599ddb0d1b8a497d100e9
disial <144545367+disial@users.noreply.github.com>
2023-11-16 19:11:38 +0000

    array ints

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -15,7 +15,8 @@ int *array_range(int min, int max)
 	if (min  > max)
 		return (NULL);
 	dest = malloc(((max - min) + 1) * sizeof(int));
-	for (i = 0; min <= max;)
+	i = 0;
+	while (min <= max)
 	{
 		dest[i] = min;
 		i++;
