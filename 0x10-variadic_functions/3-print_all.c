2393aa54312f373ed5fd18e6ce21c53db2a322c4
disial <144545367+disial@users.noreply.github.com>
2023-11-17 21:20:09 +0000

    prt all

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,74 @@
+#include "variadic_functions.h"
+
+/**
+ * _char - char func
+ * @l: list
+ */
+void _char(va_list l)
+{
+	printf("%c", va_arg(l, int));
+}
+
+/**
+ * _str - str
+ * @l: list
+ */
+void _str(va_list l)
+{
+	char *str;
+
+	str = va_arg(l, char *);
+	if (!str)
+		str = "(nil)";
+	printf("%s", str);
+}
+
+/**
+ * _f - float
+ * @l: list
+ */
+void _f(va_list l)
+{
+	printf("%f", va_arg(l, double));
+}
+
+/**
+ * _int - int
+ * @l: list
+ */
+void _int(va_list l)
+{
+	printf("%d", va_arg(l, int));
+}
+
+/**
+ * print_all - print all
+ * @format: format
+ */
+void print_all(const char * const format, ...)
+{
+	unsigned int i, j;
+	va_list args;
+	char *separator;
+
+	check_format format_types[] = {
+		{ "c", _char },
+		{ "s", _str },
+		{ "f", _f },
+		{ "i", _int}
+	};
+	va_start(args, format);
+	for (i = 0, separator = ""; format != NULL && format[i / 4] != '\0';)
+	{
+		j = i % 4;
+		if (format_types[j].type[0] == format[i / 4])
+		{
+			printf("%s", separator);
+			format_types[j].f(args);
+			separator = ", ";
+		}
+		i++;
+	}
+	printf("\n");
+	va_end(args);
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -6,5 +6,19 @@
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+void _char(va_list l);
+void _int(va_list l);
+void _f(va_list l);
+void _str(va_list l);
+
+/**
+ * format_types - types
+ */
+typedef struct check_format
+{
+	char *type;
+	void (*f)();
+} check_format;
 
 #endif
