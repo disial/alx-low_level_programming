5c25a682c555fcdd3132e45c53516690733a6d5e
disial <144545367+disial@users.noreply.github.com>
2023-11-17 06:27:52 +0000

    dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -8,15 +8,15 @@
  */
 void print_dog(struct dog *d)
 {
-	if (!d)
+	if (d == NULL)
 		return;
-	if (!d->name)
+	if (d->name == NULL)
 		printf("Name: (nil)\n");
 	else
 		printf("Name: %s\n", d->name);
-	printf("age: %f\n", d->age);
+	printf("Age: %f\n", d->age);
 	if (!d->owner)
-		printf("owner: (nil)\n");
+		printf("Owner: (nil)\n");
 	else
 		printf("Owner: %s\n", d->owner);
 }
