0d0df81a21edd97a7e97e1d2079763af1f1942bc
disial <144545367+disial@users.noreply.github.com>
2023-11-18 17:08:14 +0000

    linked lists

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * print_list - print list
+ * @h: ...
+ * Return: num nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
