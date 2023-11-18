e401e50cb71cbda3c816432664acdc72f36c4a0f
disial <144545367+disial@users.noreply.github.com>
2023-11-18 22:08:53 +0000

    typedef

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -31,7 +31,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	ndog->owner = malloc(j * sizeof(ndog->owner));
 	if (ndog == NULL)
 		return (NULL);
-	for (k = 0; i < j; k++)
+	for (k = 0; k < j; k++)
 		ndog->owner[k] = owner[k];
 	return (ndog);
 }
