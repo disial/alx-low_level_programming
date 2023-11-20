979fc0776b97e6b181aa01967671e2f4d120747a
disial <144545367+disial@users.noreply.github.com>
2023-11-20 20:02:31 +0000

    free lists

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * free_listint - free list
+ * @head: list head
+ */
+void free_listint(listint_t *head)
+{
+	if (head == NULL)
+		return;
+	free_listint(head->next);
+	free(head);
+}
