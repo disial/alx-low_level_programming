0d42b1443ccb02c2f569fef22720bd02b7e4f612
disial <144545367+disial@users.noreply.github.com>
2023-11-21 18:10:29 +0000

    reverse

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * reverse_listint - reverse list
+ * @head: node head
+ * Return: ptr
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	head++;
+	return (0);
+}
--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -9,24 +9,3 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *temp, *dest;
-
-	temp = *head;
-	dest = malloc(sizeof(listint_t));
-	if (dest == NULL)
-		return (NULL);
-	dest->n = n;
-	if (index == 0)
-	{
-		dest->next = temp->next;
-		index--;
-		if (temp == NULL)
-		{
-			free(dest);
-			return (NULL);
-		}
-	}
-	dest->next = temp->next;
-	temp->next = dest;
-	return (dest);
-}
